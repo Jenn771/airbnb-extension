6e8f59ae4ee6209b48c56fc91173c88da7957adc
Jennifer Perez-Diaz <perezjennd@gmail.com>
2026-02-28 17:29:49 -0800

    Add CORS middleware to Express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import listingsRouter from './routes/listings';
 import snapshotsRouter from './routes/snapshots';
@@ -7,6 +8,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/listings', listingsRouter);
